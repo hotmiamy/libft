2006057aa7ccaa6150499720582d3e98f7bb9d22
hotmiamy <mrllps07@gmail.com>
2021-09-05 18:12:37 -0300

    bug fixed: ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: llopes-n <llopes-n@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 14:26:57 by llopes-n          #+#    #+#             */
-/*   Updated: 2021/08/30 21:29:38 by llopes-n         ###   ########.fr       */
+/*   Updated: 2021/09/05 18:11:29 by llopes-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destSize)
 		ft_memcpy(dest, src, srclen + 1);
 	else if (destSize != 0)
 	{
-		ft_memcpy(dest, src, destSize - 1);
+		ft_memcpy(dest, src, destSize);
 		dest[destSize - 1] = '\0';
 	}
 	return (srclen);
